9ff094ef7a1281dfa964f316a5362fc70d86dc38
yohei0317 <yohei@ryugaku-johokan.com>
2026-06-14 12:31:17 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
+import React from 'react';
+
 export const metadata = {
   title: '習慣化アプリ',
   description: '毎日を記録するアプリ',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ja">
       <body>{children}</body>
     </html>
-  )
+  );
 }
